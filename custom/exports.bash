e42eb654d1e3acb41c4ed1a0a988774a26e53ae2
Marco Antonio Moraes <mrcomoraes@gmail.com>
2022-02-01 15:44:36 -0300

    Adição de exports para o Kubernetes

--- custom/exports.bash
+++ custom/exports.bash
@@ -3,3 +3,8 @@ export PATH="$PYENV_ROOT/bin:$PATH:$HOME/.local/bin:$HOME/.tfenv/bin"
 export HISTCONTROL=ignoredups
 export HISTSIZE=20000
 eval "$(pyenv init --path)"
+
+### Kubernetes
+export KUBE_PS1_SYMBOL_ENABLE=false
+export KUBE_EDITOR='code -n -w'
+
