31651cc4be6711a8184d37b1569c1256837e24ac
Marco Antonio Moraes <mrcomoraes@gmail.com>
2022-03-07 12:23:14 -0300

    Adição do perfil via

--- custom/exports.bash
+++ custom/exports.bash
@@ -1,8 +1,11 @@
+# Python e Pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH:$HOME/.local/bin:$HOME/.tfenv/bin"
-export HISTCONTROL=ignoredups
-export HISTSIZE=20000
 eval "$(pyenv init --path)"
+
+# History
+export HISTCONTROL=ignoredups
+#export HISTSIZE=20000
 
 ### Kubernetes
 export KUBE_PS1_SYMBOL_ENABLE=false
