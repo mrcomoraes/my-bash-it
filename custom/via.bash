1a086b2a73d530ba46b2273da7ab42e6ddc26281
Marco Antonio Moraes <mrcomoraes@gmail.com>
2022-04-11 14:47:07 -0300

    Adição de função da VPN Via

--- custom/via.bash
+++ custom/via.bash
@@ -14,6 +14,26 @@ function via_ocp3_exibir_hpa() {
   oc get hpa -n $1 --no-headers | awk -v date="$(date "+%d/%m/%Y - %T")" '{ if($6>$4) print date,"\tMinimo: ",$4,"\tMaximo: ",$5,"\tAtual: "$6,"\tPorcentagem: ",$3,"\tDeployment: ",$1}'
 }
 
+function via_vpn {
+  case "$1" in
+    wifi)
+      INTERFACE=wlp2s0
+    ;;
+
+    cabo)
+      INTERFACE=eno1
+    ;;
+
+    *)
+      echo "Utilize $0 (cabo|wifi) como parâmetro."
+      exit 1
+    ;;
+  esac
+
+  sudo ip route add 10.0.0.0/8 via 192.168.10.91 dev "$INTERFACE"
+  sudo systemd-resolve -i "$INTERFACE" --set-dns=10.128.8.75 --set-dns=10.128.8.76
+}
+
 #function via_synckubeconfig() {
 #  rsync -rtD $HOME/.kube/ /mnt/c/Users/2160000724/Documents/MobaXterm/home/.kube/
 #}
