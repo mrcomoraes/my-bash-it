3076e0e8c71dda234e2d429931ac20abba3551aa
Marco Antonio Moraes <mrcomoraes@gmail.com>
2023-09-20 19:53:02 -0300

    history control

--- custom/exports.bash
+++ custom/exports.bash
@@ -4,12 +4,9 @@ export PATH="$PATH:$PYENV_ROOT/bin:$HOME/.local/bin:$HOME/.tfenv/bin:${KREW_ROOT
 eval "$(pyenv init --path)"
 
 # History
-export HISTCONTROL=ignoredups
-#export HISTSIZE=20000
-#export PROMPT_COMMAND='history -a'
+export HISTCONTROL=ignoreboth:erasedups
 shopt -s histappend
 
 ### Kubernetes
-#export KUBE_PS1_SYMBOL_ENABLE=false
 export KUBE_EDITOR='code -w'
 
